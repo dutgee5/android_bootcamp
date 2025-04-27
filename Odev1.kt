13814d3f0aba4ef17cc9b293a9e0699f39922e64
theDone <done.muzaffer5@gmail.com>
2025-04-27 17:19:17 +0300

    odev1 ve odev2 tamamlandi

---
+++ Odev1.kt
@@ -0,0 +1,63 @@
+package com.example.kotlin.odevler
+
+fun main(){
+    // Asagidaki degiskenlere deger atayiniz.
+
+    val sehir="NeW York"
+    val ulke= "United States"
+    val telefon="111111111111"
+    val postaKodu="34000"
+    val mail = "abc@gmail.com"
+    val meslek = "Sofware Eng."
+    val stokMiktari = 5
+    val musteriAdi = "Jones"
+    val bakiye =23
+    val dogumGunu = "1990-01-01"
+    val maas=4500
+    val medeniDurum="bekar"
+    val urunYorum="Very useful, I recommend it"
+
+    val odemeTarih="2025-26-04"
+    val odeme="Cash"
+    val siparisAdedi=1
+    val arabaModeli="520i M Sport"
+    val kitapAdi="Efendi"
+    val yayinlanmaTarihi ="2004"
+    val indirimMiktari ="%20"
+    val odaSayisi =3
+    val enlem ="28 E 58"
+    val boylam ="41 N 01"
+    val urunAdi ="headphone"
+    val yemekFiyati=120
+
+    val marka ="BMW"
+    val muzikAdi ="The sky is crying"
+    val videoSuresi="4.53"
+    val urunPuani ="4.7"
+    val resimAdi="headphone.png"
+    val dosyaFormati=".png"
+    val renk ="black"
+    val renkKodu="#000000"
+    val telefonModeli="Nokia 105"
+    val ekranBoyutu="1,8 inç"
+    val agirlik=" 79.4 gr"
+
+    val ulusalGun ="23 Nisan"
+    val tatilGunu ="carsamba"
+    val rezervasyonTarihi ="2025-01-01"
+    val sokakAdi ="1.Sk."
+    val otobusHatti="12"
+    val kalanDakika="12 dk"
+    val takipKodu ="1Icerisinde-cesitli-karakterlerin-oldugu-Yapı0"
+    val kuponSuresi="1 hour"
+    val kuponKodu="01Icerisinde-cesitli-karakterlerin-oldugu-Yapı01"
+    val faturaAdresi ="Urunler is yeri teslimi olup nakliye alici firmaya aittir"
+
+    println(sehir)
+    println(ulke)
+    /*
+    *
+    *
+    *
+    * */
+}
---
+++ Odev2.kt
@@ -0,0 +1,77 @@
+package com.example.kotlin.odevler
+
+fun main(){
+
+    // Parametre olarak girilen dereceyi fahrenhiet'a donusturdukten sonra geri donduren bir fonk
+    fun fanrenhiet(derece:Int):Double{
+        val sonuc = (derece *1.8)+32
+        return sonuc
+    }
+   println(fanrenhiet(23))
+
+    //Kenarlari parametre olarak girilen ve dikdörtgen cevresini hesaplayan fonk
+
+    fun dikdortgenCevreHesabi(uzunkenar:Int,kisaKenar:Int){
+        println(2*(uzunkenar+kisaKenar))
+    }
+    println(dikdortgenCevreHesabi(10,5))
+
+    // Parametre olarak girilen sayının faktoriyel değerini hesaplayıp geri döndüren metodu yazınız.
+    fun faktoriyel(sayi:Int):Int{
+        var toplam =1
+        for (i in sayi downTo 1){
+            toplam*=i
+        }
+        return toplam
+    }
+   println(faktoriyel(5))
+    // Parametre olarak girilen kelime içinde kaç adet a harfi olduğunu gösteren bir metod yazınız
+
+    fun harf(kelime: String): Int {
+        return kelime.count { it == 'a'}
+    }
+    println(harf("Istanbul"))
+
+    //Parametre olarak girilen kenar sayısına göre iç açılar toplamını hesaplayıp sonucu geri gönderen metod yazınız.
+    fun icAcilar(kenarSayisi: Int): Int {
+        return (kenarSayisi - 2) * 180
+    }
+    println(icAcilar(4))
+
+
+    // Parametre olarak girilen gün sayısına göre maaş hesabı yapan ve elde edilen değeri döndüren metod yazınız.
+                         /*
+                            1 günde 8 saat çalışılabilir.
+                            Çalışma saat ücreti : 10 も
+                            Mesai saat ücreti : 20も
+                            160 saat üzeri mesai sayılır.
+                        */
+    fun maas(gun: Int): Int {
+        val toplamSaat = gun * 8
+        return if (toplamSaat <= 160) {
+            toplamSaat * 10
+        } else {
+            val mesaiSaat = toplamSaat - 160
+            (160 * 10) + (mesaiSaat * 20)
+        }
+    }
+
+    println("Alinan maas :${maas(23)}")
+
+
+    //Parametre olarak girilen kota miktarına göre ücreti hesaplayarak geri döndüren metodu yazınız.
+    //  50 GB = 100 も --  Kota aşımından sonra her 1 GB, 4 'dir
+
+    fun kotaMiktari(kota: Int): Int {
+        return if (kota <= 50) {
+            100
+        } else {
+            100 + ((kota - 50) * 4)
+        }
+    }
+
+    println("Ucret : ${kotaMiktari(100)}")
+
+}
+
+
